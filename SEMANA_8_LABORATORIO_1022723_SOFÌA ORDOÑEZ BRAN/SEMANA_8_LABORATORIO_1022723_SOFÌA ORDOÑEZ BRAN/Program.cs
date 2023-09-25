c4ff3d54f435b421f3015e9208c7a75265d76c47
Sbran1703 <142233029+Sbran1703@users.noreply.github.com>
2023-09-25 17:26:17 -0600

    Add files via upload

---
+++ SEMANA_8_LABORATORIO_1022723_SOFÌA ORDOÑEZ BRAN/SEMANA_8_LABORATORIO_1022723_SOFÌA ORDOÑEZ BRAN/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese un número entero mayor que 0: ");
+        if (int.TryParse(Console.ReadLine(), out int n) && n > 0)
+        {
+            int a = 0, b = 1, c;
+            int i = 0;
+
+            Console.WriteLine("Secuencia de Fibonacci hasta el término N:");
+            Console.Write(a + " " + b + " ");
+
+            while (i < n - 2)
+            {
+                c = a + b;
+                Console.Write(c + " ");
+                a = b;
+                b = c;
+                i++;
+            }
+
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine("Debe ingresar un número entero mayor que 0.");
+        }
+    }
+}
+
---
+++ SEMANA_8_LABORATORIO_1022723_SOFÌA ORDOÑEZ BRAN/SEMANA_8_LABORATORIO_1022723_SOFÌA ORDOÑEZ BRAN/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("SEMANA_8_LABORATORIO_1022723_SOFÌA ORDOÑEZ BRAN")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SEMANA_8_LABORATORIO_1022723_SOFÌA ORDOÑEZ BRAN")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("1593d555-59e4-443e-99d5-25dfb7aed43f")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
