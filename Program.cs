35d704e60c14a0cdbfcd2ccae4a2163d81491b24
Sbran1703 <142233029+Sbran1703@users.noreply.github.com>
2023-10-09 11:13:48 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,33 +1,65 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program
+
+
+namespace L10MG1100823
 {
-    static void Main()
+    internal class Program
     {
-        Console.Write("Ingrese un número entero mayor que 0: ");
-        if (int.TryParse(Console.ReadLine(), out int n) && n > 0)
+        static void Main(string[] args)
         {
-            int a = 0, b = 1, c;
-            int i = 0;
+            Automovil automovil_1 = new Automovil();
 
-            Console.WriteLine("Secuencia de Fibonacci hasta el término N:");
-            Console.Write(a + " " + b + " ");
 
-            while (i < n - 2)
-            {
-                c = a + b;
-                Console.Write(c + " ");
-                a = b;
-                b = c;
-                i++;
-            }
 
-            Console.WriteLine();
-        }
-        else
-        {
-            Console.WriteLine("Debe ingresar un número entero mayor que 0.");
+            Console.WriteLine("Modelo:" + automovil_1.modelo);
+            Console.WriteLine("Precio:" + automovil_1.precio);
+            Console.WriteLine("Marca:" + automovil_1.marca);
+            Console.WriteLine("Disponible:" + automovil_1.disponible);
+            Console.WriteLine("tipoCambioDolar:" + automovil_1.tipoCambioDolar);
+            Console.WriteLine("descuentoAplicado:" + automovil_1.descuentoAplicado);
+
+
+
+            Console.ReadKey();
         }
     }
 }
+// Creación Clases Automovil
+public class Automovil
+{
+    //Atributos de la clase
+    public int modelo { get; set; }
+    public double precio { get; set; }
+    public string marca { get; set; }
+    public bool disponible { get; set; }
+    public double tipoCambioDolar { get; set; }
+    public double descuentoAplicado { get; set; }
 
+
+
+    // Constructores
+    public Automovil()
+    {
+        this.modelo = 2019;
+        this.precio = 10000;
+        this.marca = "";
+        this.disponible = false;
+        this.tipoCambioDolar = 7.50;
+        this.descuentoAplicado = 0.00;
+    }
+    public Automovil(int modelo, double precio, string marca, bool disponible, double tipoCambioDolar, double descuentoAplicado)
+    {
+        this.modelo = modelo;
+        this.precio = precio;
+        this.marca = marca;
+        this.disponible = disponible;
+        this.tipoCambioDolar = tipoCambioDolar;
+        this.descuentoAplicado = descuentoAplicado;
+    }
+}
