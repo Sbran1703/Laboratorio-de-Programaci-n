d91b1dd8ecc2f3141238becd186acca03ecbe27a
Sbran1703 <142233029+Sbran1703@users.noreply.github.com>
2023-10-23 10:36:24 -0600

    Add files via upload

---
+++ LAB10_SOB_1022723/LAB10_SOB_1022723/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB10SOB1022723
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Laboratorio Semana 12 - Sofía Ordoñez B");
+            Console.WriteLine("Ingrese el radio del círculo:");
+            double radio = Convert.ToDouble(Console.ReadLine());
+
+            // Objeto de la clase 
+            Circulo objCirculo = new Circulo(radio);
+
+            double perimetro = 0;
+            double area = 0;
+            double volumen = 0;
+            double pi = 3.1416;
+
+            objCirculo.CalcularGeometrica(ref perimetro, ref area, ref volumen);
+
+            // Resultados
+            Console.WriteLine("Perímetro del círculo: " + perimetro + "m");
+            Console.WriteLine("Área del círculo: " + area + "m²");
+            Console.WriteLine("Volumen de la esfera con ese radio: " + volumen + "m³");
+
+            // Esperar 
+            Console.ReadKey();
+        }
+
+        // Clase Circulo
+        internal class Circulo
+        {
+            private double radio;
+            private const double Pi = 3.1416;
+
+            public Circulo(double radio)
+            {
+                this.radio = radio;
+            }
+            private double ObtenerPerimetro()
+            {
+                return 2 * Pi * radio;
+            }
+            private double ObtenerArea()
+            {
+                return Pi * radio * radio;
+            }
+            private double ObtenerVolumen()
+            {
+                return (4 / 3) * Pi * radio * radio * radio;
+            }
+            public void CalcularGeometrica(ref double unPerimetro, ref double unArea, ref double unVolumen)
+            {
+                unPerimetro = ObtenerPerimetro();
+                unArea = ObtenerArea();
+                unVolumen = ObtenerVolumen();
+            }
+        }
+    }
+}
+
